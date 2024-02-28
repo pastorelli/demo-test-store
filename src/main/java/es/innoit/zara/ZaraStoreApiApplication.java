722dadc371b79a8e571d5d001ba77caed94b20f7
Gustavo Pastorelli <gpastorelli89@gmail.com>
2024-02-28 13:10:08 +0100

    feat: :tada: Initialize base project

---
+++ src/main/java/es/innoit/zara/ZaraStoreApiApplication.java
@@ -0,0 +1,13 @@
+package es.innoit.zara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraStoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZaraStoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/innoit/zara/ZaraStoreApiApplicationTests.java
@@ -0,0 +1,12 @@
+package es.innoit.zara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZaraStoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+
+}
